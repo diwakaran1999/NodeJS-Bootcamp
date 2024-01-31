81eb03828d9edbcb0572c593bdcf4a40312eba0a
Diwakaran Agrawal <diwakaranagr1999@gmail.com>
2024-01-31 17:47:08 +0530

    day3 complete

--- day3.js
+++ day3.js
@@ -1,8 +1,28 @@
+// const jsondata1 = '{ "name": "Alice", "age": 25, "hobbies": ["reading", "painting"] }'
+// const object = JSON.parse(jsondata1);
+// console.log(object.age)
+
+// const jsonobj = { "title": "Book", "pages": 200 };
+// const jsondata2 = JSON.stringify(jsonobj);
+// console.log(jsondata2)
+
+
 const express = require('express');
 const app = express();
 
-app.get('/',(req, res) => {
+app.get('/',(req, res) => {         //get request info from server
     res.send("hello vro hey")
 })
 
-app.listen(9989)
+app.get('/menu', (req, res) => {
+    var options = {
+        isMenu: true,
+        working: true,
+        anybug: false
+    }
+    res.send(options)
+})
+
+app.listen(9989, () => {
+    console.log("listening to port")
+})
