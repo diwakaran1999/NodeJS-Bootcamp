2c80112b49846a56c0e4d831169d4290f8a69750
Diwakaran Agrawal <diwakaranagr1999@gmail.com>
2024-01-31 16:58:31 +0530

    update

--- day3.js
+++ day3.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get((req, res) => {
-    res.send('/',"hello")
+app.get('/',(req, res) => {
+    res.send("hello vro hey")
 })
 
-app.listen(9999)
+app.listen(9989)
