91c7abf6a998e02fa78c089f36381d901f3e72ba
Diwakaran Agrawal <diwakaranagr1999@gmail.com>
2024-02-27 17:54:18 +0530

    crud on menu completed

--- Menu_NodeJs/routes/menuRoutes.js
+++ Menu_NodeJs/routes/menuRoutes.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
-
 const MenuItem = require('../models/menuItem');
-
 
 router.post('/', async (req, res) => {
     try {
         const data = req.body;
-        const newMenu = new (data);
+        const newMenu = new MenuItem(data);
         const response = await newMenu.save();
         console.log('new menu saved');
         res.status(201).json(response);
@@ -29,5 +26,60 @@ router.get('/', async (req, res) => {
     }
 
 });
+
+router.get('/:tasteType', async (req, res) => { 
+    try {
+        const tasteType = req.params.tasteType;
+        if (tasteType == 'Spicy' || tasteType == 'Salty' || tasteType == 'Sour' || tasteType == 'Sweet') {
+            const response = await MenuItem.find({ taste: tasteType });
+            console.log('filtered by taste type');
+            res.status(200).json(response);
+        }
+        else {
+            res.status(400).json({ error: 'Invalid taste' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const menuId = req.params.id;
+        const updatedData = req.body;
+
+        const response = await MenuItem.findByIdAndUpdate(menuId, updatedData, {
+            new: true,
+            runValidators: true,
+        })
+
+        if (!menuId) {
+            return res.status(400).json({ error: 'Menu Item not found' });
+        }
+        console.log('Menu Data Updated');
+        res.json(response);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Invalid Menu Id' });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const menu = req.params.id;
+        const response = await MenuItem.findByIdAndDelete(menu);
+
+        if (!response) {
+            return res.status(400).json({ error: 'Menu item not found' });
+        }
+
+        console.log('Menu item deleted successfully');
+        res.status(200).json({message: 'Menu item deleted'})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Invalid menu id' });
+    }
+})
 
 module.exports = router;
--- Menu_NodeJs/routes/personRoutes.js
+++ Menu_NodeJs/routes/personRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Person = require('../models/person')
+
 router.post('/', async (req, res) => {
     try {
         const data = req.body;
