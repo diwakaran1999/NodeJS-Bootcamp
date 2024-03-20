9c2644197a71d6892afb6b4f3a19826d571226c0
Diwakaran Agrawal <70992885+diwakaran1999@users.noreply.github.com>
2024-03-21 02:31:10 +0530

    Update db.js

--- Menu_NodeJs/db.js
+++ Menu_NodeJs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // const mongoURL = 'mongodb://127.0.0.1:27017/test_data';
-const mongoURL = 'mongodb+srv://diwakaranagr1999:node_bootcamp@cluster0.ri3bn6g.mongodb.net/';
+const mongoURL = 'mongodb+srv://<user>:<password>@cluster0.ri3bn6g.mongodb.net/';
 
 
 
