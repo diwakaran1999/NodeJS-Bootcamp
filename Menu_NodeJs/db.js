db575262141c2b24a1d0845c2c491b19c48684b4
Diwakaran Agrawal <diwakaranagr1999@gmail.com>
2024-03-24 18:01:48 +0530

    change

--- Menu_NodeJs/db.js
+++ Menu_NodeJs/db.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 // const mongoURL = 'mongodb://127.0.0.1:27017/test_data';
-const mongoURL = 'mongodb+srv://diwakaranagr1999:node_bootcamp@cluster0.ri3bn6g.mongodb.net/';
-
-
+const mongoURL = process.env.MONGO_URI;
 
 mongoose.connect(mongoURL);
 const db = mongoose.connection;
