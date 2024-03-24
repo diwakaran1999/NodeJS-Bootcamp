5a67057f61c842abfbf25b4a09e7087759002ec8
Diwakaran Agrawal <diwakaranagr1999@gmail.com>
2024-03-24 18:02:47 +0530

    update db

--- Menu_NodeJs/db.js
+++ Menu_NodeJs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 // const mongoURL = 'mongodb://127.0.0.1:27017/test_data';
-const mongoURL = process.env.MONGO_URI;
+const mongoURL = process.env.MONGO_URL;
 
 mongoose.connect(mongoURL);
 const db = mongoose.connection;
